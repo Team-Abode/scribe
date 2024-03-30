92bad5a134c65fa42d373ead9d40516c450d36c8
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-03-30 12:50:17 -0500

    Refactor the animation API

--- src/main/java/com/teamabode/sketch/core/api/animation/AnimationManager.java
+++ src/main/java/com/teamabode/sketch/core/api/animation/AnimationManager.java
@@ -1,68 +1,60 @@
 package com.teamabode.sketch.core.api.animation;
 
-import com.google.common.collect.Maps;
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
-import com.google.gson.JsonParser;
 import com.mojang.serialization.JsonOps;
 import com.teamabode.sketch.Sketch;
 import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
+import net.minecraft.Util;
 import net.minecraft.client.animation.AnimationChannel;
 import net.minecraft.client.animation.AnimationDefinition;
 import net.minecraft.client.animation.AnimationDefinition.Builder;
 import net.minecraft.client.animation.Keyframe;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
+import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
 
-import java.io.BufferedReader;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class AnimationManager extends SimplePreparableReloadListener<Map<ResourceLocation, AnimationDefinition>> implements IdentifiableResourceReloadListener {
+public class AnimationManager extends SimpleJsonResourceReloadListener implements IdentifiableResourceReloadListener {
     private static final AnimationDefinition FALLBACK_ANIMATION = AnimationDefinition.Builder.withLength(0.0f).build();
-    private static final Map<ResourceLocation, AnimationDefinition> ANIMATIONS = Maps.newHashMap();
+    private static final Gson GSON = new GsonBuilder().create();
 
-    protected Map<ResourceLocation, AnimationDefinition> prepare(ResourceManager resourceManager, ProfilerFiller profiler) {
-        Map<ResourceLocation, AnimationDefinition> registry = Maps.newHashMap();
+    private Map<ResourceLocation, AnimationDefinition> animations;
 
-        for (var resourceEntry : resourceManager.listResources("sketch_animations", location -> location.getPath().endsWith(".json")).entrySet()) {
-            ResourceLocation location = resourceEntry.getKey();
-            String path = location.getPath();
+    public AnimationManager() {
+        super(GSON, "sketch_animations");
+        this.animations = Map.of();
+    }
 
-            ResourceLocation trueLocation = new ResourceLocation(location.getNamespace(), path.substring(18, path.length() - 5));
-            Resource resource = resourceEntry.getValue();
-
-            try (BufferedReader reader = resource.openAsReader()) {
-                var result = AnimationHolder.CODEC.decode(JsonOps.INSTANCE, JsonParser.parseReader(reader));
-                AnimationDefinition animation = createAnimation(result.result().get().getFirst());
-
-                if (result.error().isPresent()) {
-                    throw new JsonParseException(result.error().get().message());
-                }
-                if (registry.put(trueLocation, animation) != null) {
-                    Sketch.LOGGER.warn("Duplicate animation found: {}", trueLocation);
-                }
+    @Override
+    protected void apply(Map<ResourceLocation, JsonElement> data, ResourceManager resourceManager, ProfilerFiller profiler) {
+        ImmutableMap.Builder<ResourceLocation, AnimationDefinition> registry = new ImmutableMap.Builder<>();
+        data.forEach((resourceLocation, jsonElement) -> {
+            try {
+                AnimationHolder animationHolder = Util.getOrThrow(AnimationHolder.CODEC.parse(JsonOps.INSTANCE, jsonElement), JsonParseException::new);
+                registry.put(resourceLocation, this.createAnimation(animationHolder));
             }
             catch (Exception exception) {
-                Sketch.LOGGER.warn("Animation could not load: {}", resourceEntry.getKey(), exception);
-                registry.put(trueLocation, FALLBACK_ANIMATION);
+                Sketch.LOGGER.error("Failed to parse custom animation {}: {}", resourceLocation, exception);
             }
+        });
+        this.animations = registry.buildOrThrow();
+    }
+
+    public AnimationDefinition getAnimation(ResourceLocation id) {
+        if (this.animations.containsKey(id)) {
+            return this.animations.get(id);
         }
-        return registry;
+        return FALLBACK_ANIMATION;
     }
 
-    protected void apply(Map<ResourceLocation, AnimationDefinition> registry, ResourceManager resourceManager, ProfilerFiller profiler) {
-        ANIMATIONS.clear();
-        ANIMATIONS.putAll(registry);
-    }
-
-    public static AnimationDefinition getAnimation(ResourceLocation id) {
-        return ANIMATIONS.get(id);
-    }
-
-    private static AnimationDefinition createAnimation(AnimationHolder data) {
+    private AnimationDefinition createAnimation(AnimationHolder data) {
         Builder builder = Builder.withLength(data.length());
         var bones = data.bones();
         bones.forEach((bone, targetMap) -> {
