599954462ddbee9a5932a5e2b14e9ce8bb28a908
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-04-18 12:24:04 -0500

    BlockEntityExtender API

--- src/main/java/com/teamabode/sketch/core/mixin/BlockEntityTypeMixin.java
+++ src/main/java/com/teamabode/sketch/core/mixin/BlockEntityTypeMixin.java
@@ -1,8 +1,6 @@
 package com.teamabode.sketch.core.mixin;
 
-import com.teamabode.sketch.core.api.misc.BlockEntityAdditions;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import com.teamabode.sketch.core.api.misc.BlockEntityExtender;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,21 +8,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Map;
-import java.util.Set;
-
 @Mixin(BlockEntityType.class)
 public class BlockEntityTypeMixin {
 
     @Inject(method = "isValid", at = @At("RETURN"), cancellable = true)
     private void isValid(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-        BlockEntityType<?> $this = BlockEntityType.class.cast(this);
-
-        if (BlockEntityAdditions.ADDITIONS.containsKey($this)) {
-            boolean hasBlock = BlockEntityAdditions.ADDITIONS.get($this).contains(state.getBlock());
-            if (hasBlock) {
-                cir.setReturnValue(hasBlock);
-            }
+        if (BlockEntityExtender.canExtend((BlockEntityType<?>) (Object) this, state)) {
+            cir.setReturnValue(true);
         }
     }
 }
